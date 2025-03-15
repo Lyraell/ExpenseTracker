bfb821b7123cbaf0beb31c45e709e3576a9b3fb6
Jessi <Jessi@DESKTOP-RUEJVJC>
2025-03-15 20:24:41 +0100

    Add project files.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ExpenseTracker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
